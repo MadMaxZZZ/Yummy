b46e6302d71eaa99d723f6d23e994659d6553b5f
Christophe <christophe.rezk@gmail.com>
2016-06-18 02:19:48 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yummy::Application.config.secret_key_base = '40df8efeddae4cde7263bc1caac86880b7be0c2cd25c44400379b69600ef0b97d22108817c23918a7863982563f11b7aecc629220edcbab95287679acbd33fea'
