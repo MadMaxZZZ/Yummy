15ba576a54f5ac731aa7d24c4d59371b0859cb3f
Christophe <christophe.rezk@gmail.com>
2016-07-28 17:45:51 +0000

    Starting to build the new.html.erb page but facing pagination errors

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,10 @@
 class PlacesController < ApplicationController
+
   def index
-    @places = Place.all.page(params[:page]).per(10)
+      @places = Place.all.page(params[:page]).per(10)
   end
+
+  def new
+  end
+
 end
