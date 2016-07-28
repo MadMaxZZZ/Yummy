bc7eb73f0d3e0a1db2ff64b519e4e2bc8f845b96
Christophe <christophe.rezk@gmail.com>
2016-07-28 17:21:15 +0000

    Setup pagination with the kaminari gem

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
@@ -19,6 +20,9 @@ module Yummy
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+config.autoload_paths += %W(#{config.root}/lib)
+
 
 #Add the fonts path
 config.assets.paths << "#{Rails.root}/app/assets/fonts"
