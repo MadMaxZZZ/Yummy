15053ce747ab1fe1ad882c1e8de5052a85693e31
Christophe <christophe.rezk@gmail.com>
2016-08-05 23:25:26 +0000

    Fixed error so that last comment shows up

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,6 +2,10 @@ class Place < ActiveRecord::Base
   belongs_to :user
   has_many :comments
   
+  def last_comment
+    self.comments.order("id ASC").last
+  end
+  
   geocoded_by :address
   after_validation :geocode
   validates :name, presence: true, length: {minimum: 3}, uniqueness: true
